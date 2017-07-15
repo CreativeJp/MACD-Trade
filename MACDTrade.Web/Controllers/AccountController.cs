702dbc9e6c56ca10d8653174f594e3ccbc01562c
Jay <jay.dpatel1993@gmail.com>
2017-07-15 18:47:44 +0530

    Initial Configuration

---
+++ MACDTrade.Web/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MACDTrade.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "jayp")
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                RedirectToAction("Index", "Home");
+            }
+            TempData["ErrMsg"] = "username or password incorrect.";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect(FormsAuthentication.LoginUrl);
+        }
+    }
+}
---
+++ MACDTrade.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MACDTrade.Web.MvcApplication" Language="C#" %>
